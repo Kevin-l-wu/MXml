cfa565139988a9fff6b23350d706e017be085591
Kevin <0630wl@163.com>
2019-06-23 00:40:03 +0800

    Just adjusting the coding style of file TestMXml.c

--- TestMXml.c
+++ TestMXml.c
@@ -430,317 +430,317 @@ int	main(int  argc, char *argv[])
 	* Check the mxmlDelete() works properly...
 	*/
 
-  for (i = 0; i < 12; i ++)
-  {
-    if (tree->child)
-      mxmlDelete(tree->child);
-    else
-    {
-      fprintf(stderr, "ERROR: Child pointer prematurely NULL on child #%d\n",
-              i + 1);
-      mxmlDelete(tree);
-      return (1);
-    }
-  }
+	for (i = 0; i < 12; i ++)
+	{
+		if (tree->child)
+			mxmlDelete(tree->child);
+		else
+		{
+			fprintf(stderr, "ERROR: Child pointer prematurely NULL on child #%d\n",
+				i + 1);
+			mxmlDelete(tree);
+			return (1);
+		}
+	}
 
-  if (tree->child)
-  {
-    fputs("ERROR: Child pointer not NULL after deleting all children.\n", stderr);
-    return (1);
-  }
+	if (tree->child)
+	{
+		fputs("ERROR: Child pointer not NULL after deleting all children.\n", stderr);
+		return (1);
+	}
 
-  if (tree->last_child)
-  {
-    fputs("ERROR: Last child pointer not NULL after deleting all children.\n", stderr);
-    return (1);
-  }
+	if (tree->last_child)
+	{
+		fputs("ERROR: Last child pointer not NULL after deleting all children.\n", stderr);
+		return (1);
+	}
 
-  mxmlDelete(tree);
+	mxmlDelete(tree);
 
- /*
-  * Open the file/string using the default (MXML_NO_CALLBACK) callback...
-  */
+	/*
+	* Open the file/string using the default (MXML_NO_CALLBACK) callback...
+	*/
 
-  if (argv[1][0] == '<')
-    tree = mxmlLoadString(NULL, argv[1], MXML_NO_CALLBACK);
-  else if ((fp = fopen(argv[1], "rb")) == NULL)
-  {
-    perror(argv[1]);
-    return (1);
-  }
-  else
-  {
-   /*
-    * Read the file...
-    */
+	if (argv[1][0] == '<')
+		tree = mxmlLoadString(NULL, argv[1], MXML_NO_CALLBACK);
+	else if ((fp = fopen(argv[1], "rb")) == NULL)
+	{
+		perror(argv[1]);
+		return (1);
+	}
+	else
+	{
+		/*
+		* Read the file...
+		*/
 
-    tree = mxmlLoadFile(NULL, fp, MXML_NO_CALLBACK);
+		tree = mxmlLoadFile(NULL, fp, MXML_NO_CALLBACK);
 
-    fclose(fp);
-  }
+		fclose(fp);
+	}
 
-  if (!tree)
-  {
-    fputs("Unable to read XML file with default callback.\n", stderr);
-    return (1);
-  }
+	if (!tree)
+	{
+		fputs("Unable to read XML file with default callback.\n", stderr);
+		return (1);
+	}
 
-  if (!strcmp(argv[1], "test.xml"))
-  {
-    const char	*text;			/* Text value */
+	if (!strcmp(argv[1], "test.xml"))
+	{
+		const char	*text;			/* Text value */
 
-   /*
-    * Verify that mxmlFindElement() and indirectly mxmlWalkNext() work
-    * properly...
-    */
+		/*
+		* Verify that mxmlFindElement() and indirectly mxmlWalkNext() work
+		* properly...
+		*/
 
-    if ((node = mxmlFindPath(tree, "group/option/keyword")) == NULL)
-    {
-      fputs("Unable to find group/option/keyword element in XML tree.\n", stderr);
-      mxmlDelete(tree);
-      return (1);
-    }
+		if ((node = mxmlFindPath(tree, "group/option/keyword")) == NULL)
+		{
+			fputs("Unable to find group/option/keyword element in XML tree.\n", stderr);
+			mxmlDelete(tree);
+			return (1);
+		}
 
-    if (node->type != MXML_TEXT)
-    {
-      fputs("No child node of group/option/keyword.\n", stderr);
-      mxmlSaveFile(tree, stderr, MXML_NO_CALLBACK);
-      mxmlDelete(tree);
-      return (1);
-    }
+		if (node->type != MXML_TEXT)
+		{
+			fputs("No child node of group/option/keyword.\n", stderr);
+			mxmlSaveFile(tree, stderr, MXML_NO_CALLBACK);
+			mxmlDelete(tree);
+			return (1);
+		}
 
-    if ((text = mxmlGetText(node, NULL)) == NULL || strcmp(text, "InputSlot"))
-    {
-      fprintf(stderr, "Child node of group/option/value has value \"%s\" instead of \"InputSlot\".\n", text ? text : "(null)");
-      mxmlDelete(tree);
-      return (1);
-    }
-  }
+		if ((text = mxmlGetText(node, NULL)) == NULL || strcmp(text, "InputSlot"))
+		{
+			fprintf(stderr, "Child node of group/option/value has value \"%s\" instead of \"InputSlot\".\n", text ? text : "(null)");
+			mxmlDelete(tree);
+			return (1);
+		}
+	}
 
-  mxmlDelete(tree);
+	mxmlDelete(tree);
 
- /*
-  * Open the file...
-  */
+	/*
+	* Open the file...
+	*/
 
-  if (argv[1][0] == '<')
-    tree = mxmlLoadString(NULL, argv[1], type_cb);
-  else if ((fp = fopen(argv[1], "rb")) == NULL)
-  {
-    perror(argv[1]);
-    return (1);
-  }
-  else
-  {
-   /*
-    * Read the file...
-    */
+	if (argv[1][0] == '<')
+		tree = mxmlLoadString(NULL, argv[1], type_cb);
+	else if ((fp = fopen(argv[1], "rb")) == NULL)
+	{
+		perror(argv[1]);
+		return (1);
+	}
+	else
+	{
+		/*
+		* Read the file...
+		*/
 
-    tree = mxmlLoadFile(NULL, fp, type_cb);
+		tree = mxmlLoadFile(NULL, fp, type_cb);
 
-    fclose(fp);
-  }
+		fclose(fp);
+	}
 
-  if (!tree)
-  {
-    fputs("Unable to read XML file.\n", stderr);
-    return (1);
-  }
+	if (!tree)
+	{
+		fputs("Unable to read XML file.\n", stderr);
+		return (1);
+	}
 
-  if (!strcmp(argv[1], "test.xml"))
-  {
-   /*
-    * Verify that mxmlFindElement() and indirectly mxmlWalkNext() work
-    * properly...
-    */
+	if (!strcmp(argv[1], "test.xml"))
+	{
+		/*
+		* Verify that mxmlFindElement() and indirectly mxmlWalkNext() work
+		* properly...
+		*/
 
-    if ((node = mxmlFindElement(tree, tree, "choice", NULL, NULL,
-                                MXML_DESCEND)) == NULL)
-    {
-      fputs("Unable to find first <choice> element in XML tree.\n", stderr);
-      mxmlDelete(tree);
-      return (1);
-    }
+		if ((node = mxmlFindElement(tree, tree, "choice", NULL, NULL,
+			MXML_DESCEND)) == NULL)
+		{
+			fputs("Unable to find first <choice> element in XML tree.\n", stderr);
+			mxmlDelete(tree);
+			return (1);
+		}
 
-    if (!mxmlFindElement(node, tree, "choice", NULL, NULL, MXML_NO_DESCEND))
-    {
-      fputs("Unable to find second <choice> element in XML tree.\n", stderr);
-      mxmlDelete(tree);
-      return (1);
-    }
-  }
+		if (!mxmlFindElement(node, tree, "choice", NULL, NULL, MXML_NO_DESCEND))
+		{
+			fputs("Unable to find second <choice> element in XML tree.\n", stderr);
+			mxmlDelete(tree);
+			return (1);
+		}
+	}
 
- /*
-  * Print the XML tree...
-  */
+	/*
+	* Print the XML tree...
+	*/
 
-  mxmlSaveFile(tree, stdout, whitespace_cb);
+	mxmlSaveFile(tree, stdout, whitespace_cb);
 
- /*
-  * Save the XML tree to a string and print it...
-  */
+	/*
+	* Save the XML tree to a string and print it...
+	*/
 
-  if (mxmlSaveString(tree, buffer, sizeof(buffer), whitespace_cb) > 0)
-  {
-    if (argc == 3)
-    {
-      fp = fopen(argv[2], "w");
-      fputs(buffer, fp);
-      fclose(fp);
-    }
-  }
+	if (mxmlSaveString(tree, buffer, sizeof(buffer), whitespace_cb) > 0)
+	{
+		if (argc == 3)
+		{
+			fp = fopen(argv[2], "w");
+			fputs(buffer, fp);
+			fclose(fp);
+		}
+	}
 
- /*
-  * Delete the tree...
-  */
+	/*
+	* Delete the tree...
+	*/
 
-  mxmlDelete(tree);
+	mxmlDelete(tree);
 
- /*
-  * Read from/write to file descriptors...
-  */
+	/*
+	* Read from/write to file descriptors...
+	*/
 
-  if (argv[1][0] != '<')
-  {
-   /*
-    * Open the file again...
-    */
+	if (argv[1][0] != '<')
+	{
+		/*
+		* Open the file again...
+		*/
 
-    if ((fd = open(argv[1], O_RDONLY | O_BINARY)) < 0)
-    {
-      perror(argv[1]);
-      return (1);
-    }
+		if ((fd = open(argv[1], O_RDONLY | O_BINARY)) < 0)
+		{
+			perror(argv[1]);
+			return (1);
+		}
 
-   /*
-    * Read the file...
-    */
+		/*
+		* Read the file...
+		*/
 
-    tree = mxmlLoadFd(NULL, fd, type_cb);
+		tree = mxmlLoadFd(NULL, fd, type_cb);
 
-    close(fd);
+		close(fd);
 
-   /*
-    * Create filename.xmlfd...
-    */
+		/*
+		* Create filename.xmlfd...
+		*/
 
-    snprintf(buffer, sizeof(buffer), "%sfd", argv[1]);
+		snprintf(buffer, sizeof(buffer), "%sfd", argv[1]);
 
-    if ((fd = open(buffer, O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0666)) < 0)
-    {
-      perror(buffer);
-      mxmlDelete(tree);
-      return (1);
-    }
+		if ((fd = open(buffer, O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0666)) < 0)
+		{
+			perror(buffer);
+			mxmlDelete(tree);
+			return (1);
+		}
 
-   /*
-    * Write the file...
-    */
+		/*
+		* Write the file...
+		*/
 
-    mxmlSaveFd(tree, fd, whitespace_cb);
+		mxmlSaveFd(tree, fd, whitespace_cb);
 
-    close(fd);
+		close(fd);
 
-   /*
-    * Delete the tree...
-    */
+		/*
+		* Delete the tree...
+		*/
 
-    mxmlDelete(tree);
-  }
+		mxmlDelete(tree);
+	}
 
- /*
-  * Test SAX methods...
-  */
+	/*
+	* Test SAX methods...
+	*/
 
-  memset(event_counts, 0, sizeof(event_counts));
+	memset(event_counts, 0, sizeof(event_counts));
 
-  if (argv[1][0] == '<')
-    mxmlSAXLoadString(NULL, argv[1], type_cb, sax_cb, NULL);
-  else if ((fp = fopen(argv[1], "rb")) == NULL)
-  {
-    perror(argv[1]);
-    return (1);
-  }
-  else
-  {
-   /*
-    * Read the file...
-    */
+	if (argv[1][0] == '<')
+		mxmlSAXLoadString(NULL, argv[1], type_cb, sax_cb, NULL);
+	else if ((fp = fopen(argv[1], "rb")) == NULL)
+	{
+		perror(argv[1]);
+		return (1);
+	}
+	else
+	{
+		/*
+		* Read the file...
+		*/
 
-    mxmlSAXLoadFile(NULL, fp, type_cb, sax_cb, NULL);
+		mxmlSAXLoadFile(NULL, fp, type_cb, sax_cb, NULL);
 
-    fclose(fp);
-  }
+		fclose(fp);
+	}
 
-  if (!strcmp(argv[1], "test.xml"))
-  {
-    if (event_counts[MXML_SAX_CDATA] != 1)
-    {
-      fprintf(stderr, "MXML_SAX_CDATA seen %d times, expected 1 times.\n",
-              event_counts[MXML_SAX_CDATA]);
-      return (1);
-    }
+	if (!strcmp(argv[1], "test.xml"))
+	{
+		if (event_counts[MXML_SAX_CDATA] != 1)
+		{
+			fprintf(stderr, "MXML_SAX_CDATA seen %d times, expected 1 times.\n",
+			event_counts[MXML_SAX_CDATA]);
+			return (1);
+		}
 
-    if (event_counts[MXML_SAX_COMMENT] != 1)
-    {
-      fprintf(stderr, "MXML_SAX_COMMENT seen %d times, expected 1 times.\n",
-              event_counts[MXML_SAX_COMMENT]);
-      return (1);
-    }
+		if (event_counts[MXML_SAX_COMMENT] != 1)
+		{
+			fprintf(stderr, "MXML_SAX_COMMENT seen %d times, expected 1 times.\n",
+			event_counts[MXML_SAX_COMMENT]);
+			return (1);
+		}
 
-    if (event_counts[MXML_SAX_DATA] != 60)
-    {
-      fprintf(stderr, "MXML_SAX_DATA seen %d times, expected 60 times.\n",
-              event_counts[MXML_SAX_DATA]);
-      return (1);
-    }
+		if (event_counts[MXML_SAX_DATA] != 60)
+		{
+			fprintf(stderr, "MXML_SAX_DATA seen %d times, expected 60 times.\n",
+			event_counts[MXML_SAX_DATA]);
+			return (1);
+		}
 
-    if (event_counts[MXML_SAX_DIRECTIVE] != 1)
-    {
-      fprintf(stderr, "MXML_SAX_DIRECTIVE seen %d times, expected 1 times.\n",
-              event_counts[MXML_SAX_DIRECTIVE]);
-      return (1);
-    }
+		if (event_counts[MXML_SAX_DIRECTIVE] != 1)
+		{
+			fprintf(stderr, "MXML_SAX_DIRECTIVE seen %d times, expected 1 times.\n",
+			event_counts[MXML_SAX_DIRECTIVE]);
+			return (1);
+		}
 
-    if (event_counts[MXML_SAX_ELEMENT_CLOSE] != 20)
-    {
-      fprintf(stderr, "MXML_SAX_ELEMENT_CLOSE seen %d times, expected 20 times.\n",
-              event_counts[MXML_SAX_ELEMENT_CLOSE]);
-      return (1);
-    }
+		if (event_counts[MXML_SAX_ELEMENT_CLOSE] != 20)
+		{
+			fprintf(stderr, "MXML_SAX_ELEMENT_CLOSE seen %d times, expected 20 times.\n",
+			event_counts[MXML_SAX_ELEMENT_CLOSE]);
+			return (1);
+		}
 
-    if (event_counts[MXML_SAX_ELEMENT_OPEN] != 20)
-    {
-      fprintf(stderr, "MXML_SAX_ELEMENT_OPEN seen %d times, expected 20 times.\n",
-              event_counts[MXML_SAX_ELEMENT_OPEN]);
-      return (1);
-    }
-  }
+		if (event_counts[MXML_SAX_ELEMENT_OPEN] != 20)
+		{
+			fprintf(stderr, "MXML_SAX_ELEMENT_OPEN seen %d times, expected 20 times.\n",
+			event_counts[MXML_SAX_ELEMENT_OPEN]);
+			return (1);
+		}
+	}
 
 #ifndef _WIN32
- /*
-  * Debug hooks...
-  */
+	/*
+	* Debug hooks...
+	*/
 
-  if (getenv("TEST_DELAY") != NULL)
-    sleep(atoi(getenv("TEST_DELAY")));
-#  ifdef __APPLE__
-  if (getenv("TEST_LEAKS") != NULL)
-  {
-    char command[1024];
+	if (getenv("TEST_DELAY") != NULL)
+		sleep(atoi(getenv("TEST_DELAY")));
+#ifdef __APPLE__
+	if (getenv("TEST_LEAKS") != NULL)
+	{
+		char command[1024];
 
-    snprintf(command, sizeof(command), "leaks %d", (int)getpid());
-    if (system(command))
-      puts("Unable to check for leaks.");
-  }
-#  endif /* __APPLE__ */
+		snprintf(command, sizeof(command), "leaks %d", (int)getpid());
+		if (system(command))
+			puts("Unable to check for leaks.");
+	}
+#endif /* __APPLE__ */
 #endif /* !_WIN32 */
 
- /*
-  * Return...
-  */
-#endif
-  return (0);
+	/*
+	* Return...
+	*/
+
+	return (0);
 }
 
 
@@ -784,24 +784,24 @@ sax_cb(mxml_node_t      *node,		/* I - Current node */
 mxml_type_t				/* O - Data type */
 type_cb(mxml_node_t *node)		/* I - Element node */
 {
-  const char	*type;			/* Type string */
+	const char	*type;			/* Type string */
 
 
- /*
-  * You can lookup attributes and/or use the element name, hierarchy, etc...
-  */
+	/*
+	* You can lookup attributes and/or use the element name, hierarchy, etc...
+	*/
 
-  if ((type = mxmlElementGetAttr(node, "type")) == NULL)
-    type = node->value.element.name;
+	if ((type = mxmlElementGetAttr(node, "type")) == NULL)
+		type = node->value.element.name;
 
-  if (!strcmp(type, "integer"))
-    return (MXML_INTEGER);
-  else if (!strcmp(type, "opaque") || !strcmp(type, "pre"))
-    return (MXML_OPAQUE);
-  else if (!strcmp(type, "real"))
-    return (MXML_REAL);
-  else
-    return (MXML_TEXT);
+	if (!strcmp(type, "integer"))
+		return (MXML_INTEGER);
+	else if (!strcmp(type, "opaque") || !strcmp(type, "pre"))
+		return (MXML_OPAQUE);
+	else if (!strcmp(type, "real"))
+		return (MXML_REAL);
+	else
+		return (MXML_TEXT);
 }
 
 
@@ -814,84 +814,84 @@ const char *				/* O - Whitespace string or NULL */
 whitespace_cb(mxml_node_t *node,	/* I - Element node */
               int         where)	/* I - Open or close tag? */
 {
-  mxml_node_t	*parent;		/* Parent node */
-  int		level;			/* Indentation level */
-  const char	*name;			/* Name of element */
-  static const char *tabs = "\t\t\t\t\t\t\t\t";
-					/* Tabs for indentation */
+	mxml_node_t	*parent;		/* Parent node */
+	int		level;			/* Indentation level */
+	const char	*name;			/* Name of element */
+	static const char *tabs = "\t\t\t\t\t\t\t\t";
+		/* Tabs for indentation */
 
 
- /*
-  * We can conditionally break to a new line before or after any element.
-  * These are just common HTML elements...
-  */
+	/*
+	* We can conditionally break to a new line before or after any element.
+	* These are just common HTML elements...
+	*/
 
-  name = node->value.element.name;
+	name = node->value.element.name;
 
-  if (!strcmp(name, "html") || !strcmp(name, "head") || !strcmp(name, "body") ||
-      !strcmp(name, "pre") || !strcmp(name, "p") ||
-      !strcmp(name, "h1") || !strcmp(name, "h2") || !strcmp(name, "h3") ||
-      !strcmp(name, "h4") || !strcmp(name, "h5") || !strcmp(name, "h6"))
-  {
-   /*
-    * Newlines before open and after close...
-    */
+	if (!strcmp(name, "html") || !strcmp(name, "head") || !strcmp(name, "body") ||
+		!strcmp(name, "pre") || !strcmp(name, "p") ||
+		!strcmp(name, "h1") || !strcmp(name, "h2") || !strcmp(name, "h3") ||
+		!strcmp(name, "h4") || !strcmp(name, "h5") || !strcmp(name, "h6"))
+	{
+	/*
+	* Newlines before open and after close...
+	*/
 
-    if (where == MXML_WS_BEFORE_OPEN || where == MXML_WS_AFTER_CLOSE)
-      return ("\n");
-  }
-  else if (!strcmp(name, "dl") || !strcmp(name, "ol") || !strcmp(name, "ul"))
-  {
-   /*
-    * Put a newline before and after list elements...
-    */
+		if (where == MXML_WS_BEFORE_OPEN || where == MXML_WS_AFTER_CLOSE)
+		return ("\n");
+	}
+	else if (!strcmp(name, "dl") || !strcmp(name, "ol") || !strcmp(name, "ul"))
+	{
+		/*
+		* Put a newline before and after list elements...
+		*/
 
-    return ("\n");
-  }
-  else if (!strcmp(name, "dd") || !strcmp(name, "dt") || !strcmp(name, "li"))
-  {
-   /*
-    * Put a tab before <li>'s, <dd>'s, and <dt>'s, and a newline after them...
-    */
+		return ("\n");
+	}
+	else if (!strcmp(name, "dd") || !strcmp(name, "dt") || !strcmp(name, "li"))
+	{
+		/*
+		* Put a tab before <li>'s, <dd>'s, and <dt>'s, and a newline after them...
+		*/
 
-    if (where == MXML_WS_BEFORE_OPEN)
-      return ("\t");
-    else if (where == MXML_WS_AFTER_CLOSE)
-      return ("\n");
-  }
-  else if (!strncmp(name, "?xml", 4))
-  {
-    if (where == MXML_WS_AFTER_OPEN)
-      return ("\n");
-    else
-      return (NULL);
-  }
-  else if (where == MXML_WS_BEFORE_OPEN ||
-           ((!strcmp(name, "choice") || !strcmp(name, "option")) &&
-	    where == MXML_WS_BEFORE_CLOSE))
-  {
-    for (level = -1, parent = node->parent;
-         parent;
-	 level ++, parent = parent->parent);
+		if (where == MXML_WS_BEFORE_OPEN)
+			return ("\t");
+		else if (where == MXML_WS_AFTER_CLOSE)
+			return ("\n");
+	}
+	else if (!strncmp(name, "?xml", 4))
+	{
+		if (where == MXML_WS_AFTER_OPEN)
+			return ("\n");
+		else
+			return (NULL);
+	}
+	else if (where == MXML_WS_BEFORE_OPEN ||
+		((!strcmp(name, "choice") || !strcmp(name, "option")) &&
+		where == MXML_WS_BEFORE_CLOSE))
+	{
+		for (level = -1, parent = node->parent;
+			parent;
+			level ++, parent = parent->parent);
 
-    if (level > 8)
-      level = 8;
-    else if (level < 0)
-      level = 0;
+			if (level > 8)
+				level = 8;
+			else if (level < 0)
+				level = 0;
 
-    return (tabs + 8 - level);
-  }
-  else if (where == MXML_WS_AFTER_CLOSE ||
-           ((!strcmp(name, "group") || !strcmp(name, "option") ||
-	     !strcmp(name, "choice")) &&
-            where == MXML_WS_AFTER_OPEN))
-    return ("\n");
-  else if (where == MXML_WS_AFTER_OPEN && !node->child)
-    return ("\n");
+		return (tabs + 8 - level);
+	}
+	else if (where == MXML_WS_AFTER_CLOSE ||
+		((!strcmp(name, "group") || !strcmp(name, "option") ||
+		!strcmp(name, "choice")) &&
+		where == MXML_WS_AFTER_OPEN))
+		return ("\n");
+	else if (where == MXML_WS_AFTER_OPEN && !node->child)
+		return ("\n");
 
- /*
-  * Return NULL for no added whitespace...
-  */
+	/*
+	* Return NULL for no added whitespace...
+	*/
 
-  return (NULL);
+	return (NULL);
 }
